69fe9c75dfe8b2e88626d56e4a80d3f41da56434
Gintautas Plonis <gplonis@gmail.com>
2018-11-09 12:44:59 +0200

    Prettify

--- src/components/Card.js
+++ src/components/Card.js
@@ -33,11 +33,12 @@ class Card extends React.Component {
 
     return (
       <div className="card">
-        <div className="card__image" style={{ backgroundImage: `url(${getImageUrl(poster_path)})` }} />
+        <div
+          className="card__image"
+          style={{ backgroundImage: `url(${getImageUrl(poster_path)})` }}
+        />
 
-        <div className="card__title">
-          {original_title}
-        </div>
+        <div className="card__title">{original_title}</div>
 
         <div className="card__like">
           <i className="fa fa-heart-o" />
@@ -45,24 +46,22 @@ class Card extends React.Component {
 
         <div className="card__subtitle">
           <span>{release_date}</span>
-          <span>{vote_average} ({vote_count} votes)</span>
+          <span>
+            {vote_average} ({vote_count} votes)
+          </span>
         </div>
 
-        {summaryShowing
-          ? (
-            <div className="card-info">
-              <div className="card-info__header">Summary</div>
-              <div className="card-info__description">
-                {overview}
-              </div>
-            </div>
-          )
-          : null
-        }
+        {summaryShowing ? (
+          <div className="card-info">
+            <div className="card-info__header">Summary</div>
+            <div className="card-info__description">{overview}</div>
+          </div>
+        ) : null}
 
-        <div className="button" onClick={() => this.toggleSummary()}>Show summary</div>
+        <div className="button" onClick={() => this.toggleSummary()}>
+          Show summary
+        </div>
       </div>
-
     );
   }
 }
