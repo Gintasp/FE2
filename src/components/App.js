ab7430ab68192bf7f969953cfbcf989cb835f879
Gintautas Plonis <gplonis@gmail.com>
2018-11-09 13:19:18 +0200

    Fetch and display genres

--- src/components/App.js
+++ src/components/App.js
@@ -9,30 +9,53 @@ class App extends React.Component {
 
     this.state = {
       movieList: [],
-    }
+      genreList: [],
+    };
   }
 
   componentDidMount() {
     this.requestPopularMovies();
+    this.requestGenreList();
   }
 
   requestPopularMovies = () => {
     axios
       .get(endpoints.mostPopularMovies())
-      .then((response) => {
+      .then(response => {
         this.setState({
           movieList: response.data.results,
         });
       })
-      .catch((error) => console.log(error.response));
+      .catch(error => console.log(error.response));
+  };
+
+  requestGenreList = () => {
+    axios
+      .get(endpoints.genres())
+      .then(response => {
+        console.log(response);
+        this.setState({
+          genreList: response.data.genres,
+        });
+      })
+      .catch(error => console.log(error.response));
   };
 
   render() {
-    const { movieList } = this.state;
+    const { movieList, genreList } = this.state;
 
     return (
       <React.Fragment>
-        {movieList.map((movie) => (
+        {genreList ? (
+          <ul className="genre__list">
+            {genreList.map(genre => (
+              <li key={genre.id} className="genre__item">
+                {genre.name}
+              </li>
+            ))}
+          </ul>
+        ) : null}
+        {movieList.map(movie => (
           <Card key={movie.id} data={movie} />
         ))}
       </React.Fragment>
